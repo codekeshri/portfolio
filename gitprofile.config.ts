9a15cc538cb91fc9960015b0dd3c82e5743ea497
Arvind Keshri <arvidce@gmail.com>
2026-08-16 14:33:24 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -45,7 +45,7 @@ const CONFIG = {
           description:
             'Advanced Ecommerce Backend at scale in Spring Boot using Eureka, Kafka, API Gateway & Docker handling real-time order processing, inventory management and secure payments',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+            'https://images.unsplash.com/photo-1664455340023-214c33a9d0bd?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
           link: 'https://github.com/codekeshri/ecommerce',
         },
       ],
